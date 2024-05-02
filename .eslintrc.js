610aa3230b46a52c107c429a5cf60d3f25106870
warleyzee <warleyzee@hotmail.com>
2024-05-02 09:42:15 +0100

    created fake form and created the css

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,30 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es6: true,
   },
-  extends: 'airbnb',
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
   ],
-  parse: 'babel-eslint',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: [
+    'react',
+  ],
   rules: {
+    'react/jsx-filename-extension': 0,
+    'react/state-in-constructor': 0,
+    'react/forbid-prop-types': 0,
   },
 };
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 //form
-import { FaPlus } from 'react-icons/fa'
+import { FaPlus } from 'react-icons/fa';
+
+//Tasks
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
 import './Main.css';
 
 export default class Main extends Component {
   state = {
     newTask: "",
+    tasks: [
+      'Breackfast',
+      'Lunch',
+      'Dinner'
+    ]
   };
 
   handleChange = (e) => {
@@ -15,20 +24,33 @@ export default class Main extends Component {
     });
   }
   render() {
-    const { newTask } = this.state;
+    const { newTask, tasks } = this.state;
     return (
       <div className='main'>
         <h1> TASK LIST </h1>
         <form action="#" className='form'>
           <input
-              onChange={this.handleChange}
-              type="text"
-              value={ newTask }
-           />
+            onChange={this.handleChange}
+            type="text"
+            value={newTask}
+          />
           <button type='submit'>
             <FaPlus />
           </button>
         </form>
+
+        <ul className="tasks">
+          {tasks.map((tasks) => (
+            <li key={tasks}>
+              {tasks}
+              <div>
+              <FaEdit className="edit"/>
+              <FaWindowClose className="delete"/>
+              </div>
+              </li>
+          ))}
+
+        </ul>
 
       </div>
     );

aa0a51e7246d912bf707e1c67917f3625900790b
warleyzee <warleyzee@hotmail.com>
2024-05-08 09:11:08 +0100

    Last commit, finish project "Task List"

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,24 +11,81 @@ import './Main.css';
 export default class Main extends Component {
   state = {
     newTask: "",
-    tasks: [
-      'Breackfast',
-      'Lunch',
-      'Dinner'
-    ]
+    tasks: [],
+    index: -1,
   };
+  componentDidMount(){
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+
+    if(!tasks) return;
+
+    this.setState({tasks});
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    const { tasks } = this.state;
+
+    if(tasks === prevState.tasks) return;
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tasks, index } = this.state;
+    let { newTask } = this.state;
+    newTask = newTask.trim();
+
+    if (tasks.indexOf(newTask) !== -1) return;
+
+    const newTasks = [...tasks];
+
+    if (index === -1) {
+      this.setState({
+        tasks: [...newTasks, newTask],
+        newTask: '',
+      });
+    } else {
+      newTasks[index] = newTask;
+
+      this.setState({
+        tasks: [...newTasks],
+        index: -1,
+      })
+    }
+  }
 
   handleChange = (e) => {
     this.setState({
       newTask: e.target.value,
     });
   }
+  handleEdit = (e, index) => {
+    const { tasks } = this.state;
+
+    this.setState({
+      index,
+      newTask: tasks[index],
+    });
+  }
+
+  handleDelete = (e, index) => {
+    const { tasks } = this.state;
+    const newTasks = [...tasks];
+
+    newTasks.splice(index, 1);
+
+    this.setState({
+      tasks: [...newTasks],
+    })
+  }
+
   render() {
     const { newTask, tasks } = this.state;
     return (
       <div className='main'>
         <h1> TASK LIST </h1>
-        <form action="#" className='form'>
+        <form onSubmit={this.handleSubmit} action="#" className='form'>
           <input
             onChange={this.handleChange}
             type="text"
@@ -40,19 +97,22 @@ export default class Main extends Component {
         </form>
 
         <ul className="tasks">
-          {tasks.map((tasks) => (
+          {tasks.map((tasks, index) => (
             <li key={tasks}>
               {tasks}
-              <div>
-              <FaEdit className="edit"/>
-              <FaWindowClose className="delete"/>
-              </div>
-              </li>
+              <span>
+                <FaEdit className="edit"
+                  onClick={(e) => this.handleEdit(e, index)}
+                />
+                <FaWindowClose className="delete"
+                  onClick={(e) => this.handleDelete(e, index)}
+                />
+              </span>
+            </li>
           ))}
-
         </ul>
-
       </div>
     );
   }
 }
+
